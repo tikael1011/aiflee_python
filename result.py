a341b04d126ddcaf5c4d61138b544f985760e089
tikael1011 <Qian@Aeoluss-MacBook-Pro.local>
2017-03-23 02:50:57 -0400

    app

--- result.py
+++ result.py
@@ -12,51 +12,58 @@ from glob import glob
 
 flag = True
 
+'''
 while(flag):
 	while(1):
+'''
 
-		hallwayname = input("Input hallwayname or Type exit to exit: ")
+'''
+hallwayname = input("Input hallwayname or Type exit to exit: ")
 
-		if(hallwayname == 'exit'):
-			flag = False
-			break
-		locationname = input("Input datalocation: ")
 
-		file_open = open('data_filtered.txt','r')
+if(hallwayname.lower() == 'exit'):
+	flag = False
+	break
+locationname = input("Input datalocation: ")
 
-		try:
-			input_open = open('/Users/Qian/Desktop/wifidata/AVG/'+hallwayname +'/'+locationname+".txt",'r')
-		except:
-			print("point does not exist, please try again")
-			break
+file_open = open('data_filtered.txt','r')
 
-		t = time()
+try:
+	input_open = open('/Users/Qian/Desktop/wifidata/AVG/'+hallwayname.upper() +'/'+locationname+".txt",'r')
+except:
+	print("point does not exist, please try again")
+	break
+'''
+t = time()
 
-		all_data = [x.strip() for x in file_open.readlines()]
-		all_input = [x.strip() for x in input_open.readlines()]
+file_open = open('data_filtered.txt','r')
+input_open = open('demo.txt','r')
 
-		cad = []
+all_data = [x.strip() for x in file_open.readlines()]
+all_input = [x.strip() for x in input_open.readlines()]
 
-		i = 0
+cad = []
 
-		for element in all_input:
-			if (i >= 20): break
-			idata = element.split(' ')
-			iname = idata[0]
-			ivalue = float(idata[1])
-			if (ivalue <= -85.0):
-				continue
-			i = i+ 1
-			for base in all_data:
-				jdata = base.split(',')
-				jname = jdata[0]
-				jvalue = float(jdata[1])
-				if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
-					cad.append(jdata[2])
+i = 0
 
-		result = [loc for loc, count in Counter(cad).most_common(1)]
-		print("Based on the data you input, you are at {}".format(result))
-		print(time()-t)
+for element in all_input:
+	if (i >= 20): break
+	idata = element.split(',')
+	iname = idata[0]
+	ivalue = int(idata[3]) # was 1 for the training data and float cast
+	if (ivalue <= -85.0):
+		continue
+	i = i + 1
+	for base in all_data:
+		jdata = base.split(',')
+		jname = jdata[0]
+		jvalue = float(jdata[1])
+		if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
+			cad.append(jdata[2])
+
+result = [loc for loc, count in Counter(cad).most_common(1)]
+print("Based on the data you input, you are at {}".format(result))
+print("Time spent on computation: " + "{:.3f}".format(time()-t) + 's')
 
 ####****
 # if __name__ == '__main__':
