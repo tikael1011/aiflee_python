3da36ca5d22dc64eaa3dbda70fa6a584c98b699b
tikael1011 <tikael1011@users.noreply.github.com>
2017-03-28 17:55:46 -0400

    Update result_on_real.py

--- result_on_real.py
+++ result_on_real.py
@@ -3,3 +3,82 @@
 # transpose all the sub dir or just edit-save-run 19 times?
 # how do you define error?
 # CDF?
+
+
+#UTF-8 Jie
+
+import os
+import os.path
+import statistics
+import math
+
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+from collections import Counter
+from time import time
+from glob import glob
+
+
+#### PASS ####
+
+flag = True
+
+
+while(flag):
+	while(1):
+
+		hallwayname = input("Input hallwayname or Type exit to exit: ")
+
+
+		if(hallwayname.lower() == 'exit'):
+			flag = False
+			break
+		locationname = input("Input datalocation: ")
+
+		file_open = open('data_filtered.txt','r')
+
+		try:
+			input_open = open('/Users/Qian/Desktop/wifidata/'+ hallwayname +'/'+locationname+".txt",'r')
+		except:
+			print("point does not exist, please try again")
+			break
+
+		t = time()
+
+		#file_open = open('data_filtered.txt','r')
+		#input_open = open('demo.txt','r')
+
+		all_data = [x.strip() for x in file_open.readlines()]
+		all_input = [x.strip() for x in input_open.readlines()]
+
+		#print(type(all_input))
+
+		cad = []
+
+		i = 0
+
+		for element in all_input:
+			if(element == ""): continue
+			if(element == '****####****'): break
+			if (i >= 20): break
+			idata = element.split()
+			#print(idata)
+			
+			iname = idata[0]
+			ivalue = int(idata[3]) # was 1 for the training data and float cast
+			if (ivalue <= -85.0):
+				continue
+			i = i + 1
+			for base in all_data:
+				jdata = base.split(',')
+				jname = jdata[0]
+				jvalue = float(jdata[1])
+				if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
+					cad.append(jdata[2])
+
+		result = [loc for loc, count in Counter(cad).most_common(1)]
+		print("Based on the data you input, you are at {}".format(result))
+		print("Time spent on computation: " + "{:.3f}".format(time()-t) + 's')
+
