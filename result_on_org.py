5f84e904794efe994077ec5ea05154d6ab497df4
tikael1011 <tikael1011@users.noreply.github.com>
2017-03-28 13:15:52 -0400

    Update result_on_org.py

--- result_on_org.py
+++ result_on_org.py
@@ -4,68 +4,65 @@ from collections import Counter
 from time import time
 from glob import glob
 
-#### PASS ####
-# for dirpath, dirnames, filenames in os.walk("."):
-#     for filename in [f for f in filenames if f.endswith(".txt")]:
-#         print os.path.join(dirpath, filename)
+
 #### PASS ####
 
 flag = True
 
-'''
+
 while(flag):
 	while(1):
-'''
 
-'''
-hallwayname = input("Input hallwayname or Type exit to exit: ")
+		hallwayname = input("Input hallwayname or Type exit to exit: ")
 
 
-if(hallwayname.lower() == 'exit'):
-	flag = False
-	break
-locationname = input("Input datalocation: ")
+		if(hallwayname.lower() == 'exit'):
+			flag = False
+			break
+		locationname = input("Input datalocation: ")
 
-file_open = open('data_filtered.txt','r')
+		file_open = open('data_filtered.txt','r')
 
-try:
-	input_open = open('/Users/Qian/Desktop/wifidata/AVG/'+hallwayname.upper() +'/'+locationname+".txt",'r')
-except:
-	print("point does not exist, please try again")
-	break
-'''
-t = time()
+		try:
+			input_open = open('/Users/Qian/Desktop/wifidata/'+ hallwayname +'/'+locationname+".txt",'r')
+		except:
+			print("point does not exist, please try again")
+			break
 
-file_open = open('data_filtered.txt','r')
-input_open = open('demo.txt','r')
+		t = time()
 
-all_data = [x.strip() for x in file_open.readlines()]
-all_input = [x.strip() for x in input_open.readlines()]
+		#file_open = open('data_filtered.txt','r')
+		#input_open = open('demo.txt','r')
 
-cad = []
+		all_data = [x.strip() for x in file_open.readlines()]
+		all_input = [x.strip() for x in input_open.readlines()]
 
-i = 0
+		#print(type(all_input))
 
-for element in all_input:
-	if (i >= 20): break
-	idata = element.split(',')
-	iname = idata[0]
-	ivalue = int(idata[3]) # was 1 for the training data and float cast
-	if (ivalue <= -85.0):
-		continue
-	i = i + 1
-	for base in all_data:
-		jdata = base.split(',')
-		jname = jdata[0]
-		jvalue = float(jdata[1])
-		if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
-			cad.append(jdata[2])
+		cad = []
 
-result = [loc for loc, count in Counter(cad).most_common(1)]
-print("Based on the data you input, you are at {}".format(result))
-print("Time spent on computation: " + "{:.3f}".format(time()-t) + 's')
+		i = 0
 
-####****
-# if __name__ == '__main__':
-# 	main()
-####****
+		for element in all_input:
+			if(element == ""): continue
+			if(element == '****####****'): break
+			if (i >= 20): break
+			idata = element.split()
+			#print(idata)
+			
+			iname = idata[0]
+			ivalue = int(idata[3]) # was 1 for the training data and float cast
+			if (ivalue <= -85.0):
+				continue
+			i = i + 1
+			for base in all_data:
+				jdata = base.split(',')
+				jname = jdata[0]
+				jvalue = float(jdata[1])
+				if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
+					cad.append(jdata[2])
+
+		result = [loc for loc, count in Counter(cad).most_common(1)]
+		print("Based on the data you input, you are at {}".format(result))
+		print("Time spent on computation: " + "{:.3f}".format(time()-t) + 's')
+
