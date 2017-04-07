97953235e92209b46edfe751eb628db2c69d084d
tikael1011 <Qian@Aeoluss-MacBook-Pro.local>
2017-04-06 22:09:57 -0400

    without multiT

--- server_init.py
+++ server_init.py
@@ -2,39 +2,52 @@ import os
 import os.path
 from collections import Counter
 import glob
+import threading
+import time
 
-#flag = True
+'''
+class server_init(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+
+'''
+while(1):
+	file_open = open('/home/student/data_filtered.txt','r')
+	input_path = glob.glob('*.txt')
+
+	all_data = [x.strip() for x in file_open.readlines()]
 
 
-file_open = open('/home/student/data_filtered.txt','r')
-input_path = glob.glob('*.txt')
 
-all_data = [x.strip() for x in file_open.readlines()]
+	for files in input_path:
+		cad = []
+		i = 0
+		f = open(files,'r')
+		input_info = [x.strip() for x in f.readlines()]
+		for element in input_info:
+			if (i >= 20): break
+			idata = element.split(',')
+			iname = idata[0]
+			ivalue = int(idata[1])
+			if (ivalue <= -85.0):
+				continue
+			i = i + 1
+			for base in all_data:
+				jdata = base.split(',')
+				jname = jdata[0]
+				jvalue = float(jdata[1])
+				if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
+					cad.append(jdata[2])
 
-
-
-for files in input_path:
-	cad = []
-	i = 0
-	f = open(files,'r')
-	input_info = [x.strip() for x in f.readlines()]
-	for element in input_info:
-		if (i >= 20): break
-		idata = element.split(',')
-		iname = idata[0]
-		ivalue = int(idata[1])
-		if (ivalue <= -85.0):
-			continue
-		i = i + 1
-		for base in all_data:
-			jdata = base.split(',')
-			jname = jdata[0]
-			jvalue = float(jdata[1])
-			if (iname == jname and (jvalue-2 <= ivalue and ivalue-2 <= jvalue )):
-				cad.append(jdata[2])
-
-	result = [loc for loc, count in Counter(cad).most_common(1)]
-	o = open('/home/student/location/'+files,'a')
-	o.write(result[0])
-	f.close()
-	o.close()
+		result = [loc for loc, count in Counter(cad).most_common(1)]
+		try:
+			o = open('/home/student/location/'+files,'w')
+			o.write(result[0])
+		except:
+			o = open('/home/student/location/'+files,'a')
+			o.write(result[0])
+		f.close()
+		o.close()
+	file_open.close
+	time.sleep(0.1)
